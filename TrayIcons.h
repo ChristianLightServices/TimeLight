917323c0989f6801541d248e469b94716b2fab2e
Loren Burkholder <computersemiexpert@outlook.com>
2021-07-26 11:29:48 -0400

    Switch to a class-based system for tray icons

---
+++ TrayIcons.h
@@ -0,0 +1,42 @@
+#ifndef TRAYICONS_H
+#define TRAYICONS_H
+
+#include <QObject>
+#include <QSystemTrayIcon>
+#include <QTimer>
+
+#include "ClockifyManager.h"
+#include "ClockifyUser.h"
+
+class TrayIcons : public QObject
+{
+	Q_OBJECT
+public:
+	explicit TrayIcons(const QSharedPointer<ClockifyManager> &manager, const QSharedPointer<ClockifyUser> &user, QObject *parent = nullptr);
+	~TrayIcons();
+
+	void show();
+
+signals:
+
+private slots:
+	void updateTrayIcons();
+	void getNewProjectId();
+	void getNewApiKey();
+
+private:
+	void setUpTrayIcons();
+
+	QSystemTrayIcon *m_clockifyRunning;
+	QSystemTrayIcon *m_runningJob;
+
+	QSharedPointer<ClockifyManager> m_manager;
+	QSharedPointer<ClockifyUser> m_user;
+
+	QString m_defaultProjectId;
+	QString m_apiKey;
+
+	QTimer m_eventLoop;
+};
+
+#endif // TRAYICONS_H
