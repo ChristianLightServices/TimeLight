8d9b8ef8d7737d4ed038ebd6a05db604f6cfdd0c
Loren Burkholder <computersemiexpert@outlook.com>
2021-08-10 16:04:47 -0400

    Only switch to break/regular work if you are not already there

--- TrayIcons.cpp
+++ TrayIcons.cpp
@@ -273,7 +273,16 @@ void TrayIcons::setUpTrayIcons()
 		QDateTime start = QDateTime::currentDateTimeUtc();
 
 		if (m_user->hasRunningTimeEntry())
+		{
+			try
+			{
+				if (m_user->getRunningTimeEntry().project().id() == BREAKTIME)
+					return;
+			}
+			catch (const std::exception &) {}
+
 			start = m_user->stopCurrentTimeEntry();
+		}
 		m_user->startTimeEntry(BREAKTIME, start);
 		updateTrayIcons();
 	});
@@ -287,7 +296,16 @@ void TrayIcons::setUpTrayIcons()
 		QDateTime start = QDateTime::currentDateTimeUtc();
 
 		if (m_user->hasRunningTimeEntry())
+		{
+			try
+			{
+				if (m_user->getRunningTimeEntry().project().id() != BREAKTIME)
+					return;
+			}
+			catch (const std::exception &) {}
+
 			start = m_user->stopCurrentTimeEntry();
+		}
 		auto project = defaultProject();
 		m_user->startTimeEntry(project.id(), project.description(), start);
 		updateTrayIcons();
