3da3666c2fb4c7677e774b60061db29c90bc269a
Loren Burkholder <computersemiexpert@outlook.com>
2022-03-26 20:48:37 -0400

    Set up translations
    Only English for now.

---
+++ ClockifyTrayIcons_en_US.ts
@@ -0,0 +1,308 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>SelectDefaultProjectDialog</name>
+    <message>
+        <location filename="SelectDefaultProjectDialog.cpp" line="18"/>
+        <source>Use the project from the last task</source>
+        <translation>Use the project from the last task</translation>
+    </message>
+    <message>
+        <location filename="SelectDefaultProjectDialog.cpp" line="19"/>
+        <source>Use a specific project every time</source>
+        <translation>Use a specific project every time</translation>
+    </message>
+    <message>
+        <location filename="SelectDefaultProjectDialog.cpp" line="21"/>
+        <source>Use the description from the last task</source>
+        <translation>Use the description from the last task</translation>
+    </message>
+    <message>
+        <location filename="SelectDefaultProjectDialog.cpp" line="22"/>
+        <source>Use a specific description every time</source>
+        <translation>Use a specific description every time</translation>
+    </message>
+    <message>
+        <location filename="SelectDefaultProjectDialog.cpp" line="23"/>
+        <source>Don&apos;t use a description</source>
+        <translation>Don&apos;t use a description</translation>
+    </message>
+    <message>
+        <location filename="SelectDefaultProjectDialog.cpp" line="36"/>
+        <source>Default project settings</source>
+        <translation>Default project settings</translation>
+    </message>
+    <message>
+        <location filename="SelectDefaultProjectDialog.cpp" line="37"/>
+        <source>Default description settings</source>
+        <translation>Default description settings</translation>
+    </message>
+</context>
+<context>
+    <name>SelectDefaultWorkspaceDialog</name>
+    <message>
+        <location filename="SelectDefaultWorkspaceDialog.cpp" line="19"/>
+        <source>Select default workspace</source>
+        <translation>Select default workspace</translation>
+    </message>
+    <message>
+        <location filename="SelectDefaultWorkspaceDialog.cpp" line="20"/>
+        <source>Select the workspace you want to track time on:</source>
+        <translation>Select the workspace you want to track time on:</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcons</name>
+    <message>
+        <location filename="TrayIcons.cpp" line="47"/>
+        <location filename="TrayIcons.cpp" line="64"/>
+        <location filename="TrayIcons.cpp" line="306"/>
+        <source>API key</source>
+        <translation>API key</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="47"/>
+        <location filename="TrayIcons.cpp" line="306"/>
+        <source>Enter your Clockify API key:</source>
+        <translation>Enter your Clockify API key:</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="65"/>
+        <source>The API key is incorrect or invalid. Please enter a valid API key:</source>
+        <translation>The API key is incorrect or invalid. Please enter a valid API key:</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="95"/>
+        <source>Fatal error</source>
+        <translation>Fatal error</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="95"/>
+        <source>Could not load user!</source>
+        <translation>Could not load user!</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="105"/>
+        <source>Operation failed</source>
+        <translation>Operation failed</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="105"/>
+        <source>Could not change API key!</source>
+        <translation>Could not change API key!</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="131"/>
+        <source>Clockify is running</source>
+        <translation>Clockify is running</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="132"/>
+        <source>Clockify is not running</source>
+        <translation>Clockify is not running</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="133"/>
+        <source>You are on break</source>
+        <translation>You are on break</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="134"/>
+        <source>You are working</source>
+        <translation>You are working</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="135"/>
+        <source>You are not working</source>
+        <translation>You are not working</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="136"/>
+        <location filename="TrayIcons.cpp" line="137"/>
+        <source>You are offline</source>
+        <translation>You are offline</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="345"/>
+        <source>Select break time project</source>
+        <translation>Select break time project</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="346"/>
+        <source>Select the project used for break time:</source>
+        <translation>Select the project used for break time:</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="364"/>
+        <source>ClockifyTrayIcons %1 copyright © 2020. Licensed under the MIT license.
+
+Icons from [1RadicalOne](https://commons.wikimedia.org/wiki/User:1RadicalOne) (light icons, licensed [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)) and [Microsoft](https://github.com/microsoft/fluentui-system-icons) (power icon, licensed [MIT](https://mit-license.org)).</source>
+        <translation>ClockifyTrayIcons %1 copyright © 2020. Licensed under the MIT license.
+
+Icons from [1RadicalOne](https://commons.wikimedia.org/wiki/User:1RadicalOne) (light icons, licensed [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)) and [Microsoft](https://github.com/microsoft/fluentui-system-icons) (power icon, licensed [MIT](https://mit-license.org)).</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="382"/>
+        <source>Show license</source>
+        <translation>Show license</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="390"/>
+        <source>About </source>
+        <translation>About </translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="409"/>
+        <source>Error: could not load the license. Please read the license on [GitHub](https://github.com/ChristianLightServices/ClockifyTrayIcons/blob/master/LICENSE).</source>
+        <translation>Error: could not load the license. Please read the license on [GitHub](https://github.com/ChristianLightServices/ClockifyTrayIcons/blob/master/LICENSE).</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="422"/>
+        <source>MIT license</source>
+        <translation>MIT license</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="444"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="446"/>
+        <location filename="TrayIcons.cpp" line="457"/>
+        <location filename="TrayIcons.cpp" line="505"/>
+        <location filename="TrayIcons.cpp" line="528"/>
+        <location filename="TrayIcons.cpp" line="598"/>
+        <location filename="TrayIcons.cpp" line="624"/>
+        <source>Internet connection lost</source>
+        <translation>Internet connection lost</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="446"/>
+        <location filename="TrayIcons.cpp" line="457"/>
+        <location filename="TrayIcons.cpp" line="505"/>
+        <location filename="TrayIcons.cpp" line="528"/>
+        <location filename="TrayIcons.cpp" line="598"/>
+        <location filename="TrayIcons.cpp" line="624"/>
+        <source>The request could not be completed because the internet connection is down.</source>
+        <translation>The request could not be completed because the internet connection is down.</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="455"/>
+        <source>Stop</source>
+        <translation>Stop</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="465"/>
+        <location filename="TrayIcons.cpp" line="549"/>
+        <source>Change default project</source>
+        <translation>Change default project</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="466"/>
+        <location filename="TrayIcons.cpp" line="550"/>
+        <source>Change default workspace</source>
+        <translation>Change default workspace</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="467"/>
+        <location filename="TrayIcons.cpp" line="551"/>
+        <source>Change break time project</source>
+        <translation>Change break time project</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="468"/>
+        <location filename="TrayIcons.cpp" line="552"/>
+        <source>Change API key</source>
+        <translation>Change API key</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="469"/>
+        <location filename="TrayIcons.cpp" line="472"/>
+        <location filename="TrayIcons.cpp" line="553"/>
+        <location filename="TrayIcons.cpp" line="556"/>
+        <source>Change update interval</source>
+        <translation>Change update interval</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="473"/>
+        <location filename="TrayIcons.cpp" line="557"/>
+        <source>Choose the new interval in seconds:</source>
+        <translation>Choose the new interval in seconds:</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="486"/>
+        <location filename="TrayIcons.cpp" line="570"/>
+        <source>Show job duration at completion</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="495"/>
+        <location filename="TrayIcons.cpp" line="579"/>
+        <source>Open the Clockify website</source>
+        <translation>Open the Clockify website</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="498"/>
+        <location filename="TrayIcons.cpp" line="582"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="499"/>
+        <location filename="TrayIcons.cpp" line="583"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="500"/>
+        <location filename="TrayIcons.cpp" line="584"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="502"/>
+        <source>Break</source>
+        <translation>Break</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="525"/>
+        <source>Resume work</source>
+        <translation>Resume work</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="TrayIcons.cpp" line="670"/>
+        <source>%n minute(s)</source>
+        <translation>
+            <numerusform>%n minute</numerusform>
+            <numerusform>%n minutes</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="TrayIcons.cpp" line="672"/>
+        <source>%n hour(s) and </source>
+        <translation>
+            <numerusform>%n hour and </numerusform>
+            <numerusform>%n hours and </numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="TrayIcons.cpp" line="674"/>
+        <source> and %n second(s)</source>
+        <translation>
+            <numerusform> and %n second</numerusform>
+            <numerusform> and %n seconds</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="675"/>
+        <source>Job ended</source>
+        <translation>Job ended</translation>
+    </message>
+    <message>
+        <location filename="TrayIcons.cpp" line="676"/>
+        <source>You worked %1 on %2</source>
+        <translation>You worked %1 on %2</translation>
+    </message>
+</context>
+</TS>
